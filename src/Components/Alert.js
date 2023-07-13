68e59f88e9635362cabbf017afda1d95c96251af
Madhav Dua <madhavdua26@gmail.com>
2023-07-13 21:32:18 +0530

    minor change

--- src/Components/Alert.js
+++ src/Components/Alert.js
@@ -4,7 +4,7 @@ import React, {useEffect} from 'react'
 
 const Alert = (props) => {
     return (
-        <div className='position-absolute ' style={{zIndex:2, left:"30vw",width:"50vw"}}>
+        <div className='position-absolute ' style={{zIndex:2, left:"25vw",top:"4vh",width:"50vw"}}>
             <div className={`alert alert-${props.color} my-1`} role="alert">
                 {props.message}
             </div>
